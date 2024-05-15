d227631d03631d5dfc6ef483d25d376b890aa485
DanielWong76 <danielwong0505@gmail.com>
2024-05-15 16:54:45 -0400

    Added initial detection file

---
+++ images.py
@@ -0,0 +1,15 @@
+from PIL import Image
+from pillow_heif import register_heif_opener
+import os
+
+filepath = "training/Daniel"
+
+register_heif_opener()
+heic_files = [photo for photo in os.listdir(filepath) if '.HEIC' in photo]
+
+for photo in heic_files:
+  temp_img = Image.open(os.path.join(filepath, photo))
+  png_photo = photo.replace('.HEIC','.png')
+  temp_img.save(os.path.join(filepath, png_photo))
+
+  os.remove(os.path.join(filepath, photo))
